fc8c2088b056b8e9f5424ab282627d84256c185e
dm87 <danieljamesmcauley@gmail.com>
2016-02-11 19:28:05 +0000

    EX4 complete

--- exercise-4.js
+++ exercise-4.js
@@ -6,12 +6,15 @@ var connection = mysql.createConnection({
     database: 'addressbook'
 });
 
-connection.query('select * from Account join AddressBook on Account.id=AddressBook.accountId limit 5',function(err,results) {
+connection.query('select * from Account left outer join AddressBook on Account.id=AddressBook.accountId limit 10',function(err,results) {
     if (err)
         console.log(err);
     else
         
         results.reduce(function(acc, val){
+            if (val.name === null)
+            val.name = '--no address books--';
+            else
             console.log(val.accountId + ': ' + val.email);
             console.log('  ' + val.name);
         },[]);
